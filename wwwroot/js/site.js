e57c81bd27e48a13c68c54462640876c3db4798e
gmnelson <gmnelson@byu.edu>
2026-01-30 14:04:31 -0700

    Initial Commit -- MVC Web App for Organ Studio. Has Intro page about Hobby, and then has Calculator to determine cost of Tutoring.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// JavaScript code using JQuery that does the needed functions of the form on the Pricing.cshtml page.
+$(document).ready(function() {
+    $("#calculateButton").click(function() {
+        // Get number of hours
+        var hours = parseFloat($("#hours").val());
+        // Get rate (remove $)
+        var rate = parseFloat($("#rate").val().replace("$", ""));
+
+        if (!isNaN(hours) && !isNaN(rate)) {
+            var total = hours * rate;
+            $("#total").val("$" + total.toFixed(2));
+        } else {
+            $("#total").val("Invalid input");
+        }
+    });
+});
